23527cab49db9dbc5a476ae3810342c6dd487588
Adam Pridmore <adam_pridomre@hotmmail.com>
2023-12-21 21:41:11 +0000

    Tidy up

--- advent-of-code-scala-2023-fsharp/Day9-Part-II.fsx
+++ advent-of-code-scala-2023-fsharp/Day9-Part-II.fsx
@@ -2,19 +2,22 @@
 // 1 3 6 10 15 21
 // 10 13 16 21 30 45"""
 
-let all( predicate : 'T -> bool) (s : seq<'T>) : bool =
+
+let all(predicate : 'T -> bool) (s : seq<'T>) : bool =
   s
   |> Seq.filter(fun x -> not ( predicate x))
   |> Seq.isEmpty
 
+let isZero x = x = 0
+
 let getRows(numbers: list<int>) : list<list<int>> = 
   let nextRow(row : list<int32>) : Option<list<int32>*list<int32>> = 
-    if (row |> all (fun x -> x = 0)) then None
+    if (row |> all isZero) then None
     else 
       let nextRow = 
         row 
         |> Seq.pairwise 
-        |> Seq.map(fun (a, b) ->(b-a))
+        |> Seq.map(fun (a, b) -> b - a)
         |> Seq.toList
 
       Some(nextRow, nextRow)
@@ -23,14 +26,15 @@ let getRows(numbers: list<int>) : list<list<int>> =
     numbers
     |> List.unfold nextRow 
     
-  (numbers |> Seq.toList)::tail
+  numbers::tail
+
 let toIntList(line: string) : list<int> = 
   line.Split(" ")
   |> Seq.map System.Convert.ToInt32
   |> Seq.toList
 
-let calculateNextValue(line:list<int>) : int = 
-  let fn (state: int) (rowInt: int) : int = 
+let calculatePreviousValue(line:list<int>) : int = 
+  let previousValue(state: int) (rowInt: int) : int = 
     rowInt - state
 
   let startOfRowValues = 
@@ -39,9 +43,9 @@ let calculateNextValue(line:list<int>) : int =
     |> Seq.map(fun numbers -> Seq.head(numbers))
     |> Seq.rev
 
-  Seq.fold fn 0 startOfRowValues
+  Seq.fold previousValue 0 startOfRowValues
 
 System.IO.File.ReadAllLines("./advent-of-code-scala-2023-fsharp/Day9.input.txt")
 |> Seq.map toIntList
-|> Seq.map calculateNextValue
+|> Seq.map calculatePreviousValue
 |> Seq.sum
